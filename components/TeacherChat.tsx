27fbc98b4a23e19649984ba67dee9b6088f34246
debbipp <deborapino17@gmail.com>
2025-12-30 19:46:12 -0300

    Create TeacherChat.tsx

---
+++ components/TeacherChat.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GamesRoom() {
+  return (
+    <div>
+      <h2>Sala de Juegos</h2>
+      <p>Bienvenido a la zona lúdica de Josías School.</p>
+    </div>
+  );
+}
+
+export default GamesRoom;
