ad581e5b650fd808aaccfa03f3cce46c624340fc
debbipp <deborapino17@gmail.com>
2025-12-30 19:14:42 -0300

    Create Dashboard.tsx

---
+++ components/Dashboard.tsx
@@ -0,0 +1,11 @@
+ import React from 'react';
+   function Dashboard() {
+     return (
+       <div>
+         <h2>Bienvenida a Josías School</h2>
+         <p>Este es el panel principal.</p>
+       </div>
+     );
+   }
+   export default Dashboard;
+   `
