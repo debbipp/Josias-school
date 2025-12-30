c1498d2e17b70b599b207e0b4230160beb552722
debbipp <deborapino17@gmail.com>
2025-12-30 19:27:03 -0300

    Create SubjectViews.tsx

---
+++ components/SubjectViews.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+  function SubjectView() {
+    return (
+      <div>
+        <h2>Vista de Asignatura</h2>
+        <p>Aquí se muestra el contenido de la asignatura seleccionada.</p>
+      </div>
+    );
+  }
+  export default SubjectView;
