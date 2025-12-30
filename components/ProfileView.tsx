5e2abe81e13dd00bd28ce736298730d1536c996c
debbipp <deborapino17@gmail.com>
2025-12-30 19:55:07 -0300

    Create ProfileView.tsx

---
+++ components/ProfileView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProfileView() {
+  return (
+    <div>
+      <h2>Perfil del Usuario</h2>
+      <p>Aquí puedes ver y editar tu información personal.</p>
+    </div>
+  );
+}
+
+export default ProfileView;
