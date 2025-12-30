8c9a237c14d7582097efe12ef2d9c005bed6760e
debbipp <deborapino17@gmail.com>
2025-12-30 20:04:30 -0300

    Create EmotionSurvey.tsx

---
+++ components/EmotionSurvey.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EmotionSurvey() {
+  return (
+    <div>
+      <h2>Encuesta de Emociones</h2>
+      <p>Expresa cómo te sientes hoy y comparte tu estado emocional con la comunidad.</p>
+    </div>
+  );
+}
+
+export default EmotionSurvey;
