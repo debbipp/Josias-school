20c3a6d439fcce988f820f1db65cb72eaa70f09f
debbipp <deborapino17@gmail.com>
2025-12-30 19:49:54 -0300

    Create Login.tsx

---
+++ components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h2>Iniciar Sesión</h2>
+      <p>Ingresa tus credenciales para acceder a Josías School.</p>
+    </div>
+  );
+}
+
+export default Login;
